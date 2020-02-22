c0b81a7ca1f791c1c65d8470a72130566176cab9
yogesh rane <yorane007@outlook.com>
2020-02-22 12:13:53 +0530

    m3

--- src/TestCollection.java
+++ src/TestCollection.java
@@ -2,11 +2,12 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
 public class TestCollection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/*
 		List<List<Integer>> table = new ArrayList<List<Integer>>();
 		for(int i=0;i<10;i++) {
 			List<Integer> row = new ArrayList<Integer>();
@@ -17,6 +18,34 @@ public class TestCollection {
 		for(List<Integer> row : table)
 			System.out.println(row);
 	}
+*/
+		new TestThread().testScenario_1();
+}
+}
+
+class TestThread
+{
+	class Mytask implements Runnable
+	{
+		int x = 10;
+		public void run()
+		{
+			long id = Thread.currentThread().getId();
+			System.out.println("Mthread id "+ id + "running x=" + x);
+		}
+	}
+	void testScenario_1()
+	{
+		Mytask m = new Mytask();
+	
+		Thread t1 = new Thread(m);
+		Thread t2 = new Thread(m);
+		
+		t1.start();
+		t2.start();
+		
+	}
+	
 
 }
 
